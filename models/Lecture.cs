071ff19fe715b05646592c684f2fe47f2e90d05f
huyntt <ngtthuy@gmail.com>
2023-12-13 14:43:41 +0700

    Create Lecture.cs in folder models

---
+++ models/Lecture.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ntthuy.models
+{
+    internal class Lecture
+    {
+    }
+}
